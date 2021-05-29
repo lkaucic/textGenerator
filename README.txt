4221e38a33a23ac3e3e5aa86015e53de4e32e94c
lkaucic <luka.kaucic@etfos.hr>
2021-05-29 21:22:43 +0200

    Final stage

--- README.txt
+++ README.txt
@@ -1,1 +1,10 @@
 simple nltk text generator
+
+**code has not yet been refactored**
+
+STAGE 1: Processing the corpus
+STAGE 2: Creating bigrams
+STAGE 3: Creating Markov chain model
+STAGE 4: Generate random text
+STAGE 5: Generate full sentences
+STAGE 6: Trigrams based sentences
--- nltk_text_generator/main.py
+++ nltk_text_generator/main.py
@@ -115,6 +115,8 @@ for no_of_sentences in range(10):
     sentence.clear()
 '''
 
+# STAGE 5 TEST
+'''
 no_of_words = 1
 sentence = []
 
@@ -152,4 +154,59 @@ for i in range(10):
     print(' '.join(sentence))
     no_of_words = 0
     sentence.clear()
+'''
+
+
+# STAGE 6
+trigrams = [[corpus[i], corpus[i+1], corpus[i+2]] for i in range(len(corpus)-2)]
+trigrams2 = [[trigrams[i][0] + " " + trigrams[i][1], trigrams[i][2]] for i in range(len(trigrams))]
+trigram_heads = [trigrams2[i][0] for i in range(len(trigrams))]
+trigram_dictionary = defaultdict(list)
+for head, tail in trigrams2:
+    trigram_dictionary.setdefault(head, []).append(tail)
+
+dict3 = defaultdict(dict)
+for head in trigram_dictionary:
+    dict3[head] = Counter(trigram_dictionary[head])
+
+no_of_words = 1
+sentence = []
+
+
+def findif(pseudo_sentence):
+    words = pseudo_sentence
+    for i in range(3, len(words)):
+        if words[i].endswith(".") or words[i].endswith("?") or words[i].endswith("!"):
+            return i
+    return -1
+
+
+for i in range(10):
+    while True:
+        first_word = random.choice(trigram_heads)
+        if first_word[0].isupper() and not ("." in first_word[:-1] or "?" in first_word[:-1] or "!" in first_word[:-1]):
+            break
+    sentence.append(first_word)
+    while True:
+        population = list(set(trigram_dictionary[first_word]))
+        weights = []
+        for tail in dict3[first_word]:
+            weights.append(dict3[first_word][tail])
+        second_word = random.choices(population, weights)
+        if no_of_words >= 5:
+            if sentence[-1].endswith(".") or sentence[-1].endswith("!") or sentence[-1].endswith("?"):
+                break
+        first = str.split(first_word, " ")
+        sentence.append(second_word[0])
+        first_word = first[1] + " " + second_word[0]
+        no_of_words += 1
+
+    index = findif(sentence)
+    sentence = sentence[:index+1]
+    print(' '.join(sentence))
+    no_of_words = 0
+    sentence.clear()
+
+
+
 
